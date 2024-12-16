bcc5d446bdcccaed457351bec4ec963f6455b5d6
Gian Marco Simonazzi <gianmarco.simonazzi@studenti.unipr.it>
2024-12-16 14:29:05 +0100

    Changed ground-truth convertion.

--- scripts/extra/convert_gt_yolo.py
+++ scripts/extra/convert_gt_yolo.py
@@ -2,6 +2,7 @@ import sys
 import os
 import glob
 import cv2
+import argparse
 
 def convert_yolo_coordinates_to_voc(x_c_n, y_c_n, width_n, height_n, img_width, img_height):
   ## remove normalization given the size of the image
@@ -20,46 +21,35 @@ def convert_yolo_coordinates_to_voc(x_c_n, y_c_n, width_n, height_n, img_width, 
   bottom = int(y_c + half_height) + 1
   return left, top, right, bottom
 
-# make sure that the cwd() in the beginning is the location of the python script (so that every path makes sense)
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('--yolo_in', help='Path to the yolo results')
+parser.add_argument('--images', help='Path to the images used')
+parser.add_argument('--out', help='Output path')
+args = parser.parse_args()
+
 
 # read the class_list.txt to a list
 with open("class_list.txt") as f:
   obj_list = f.readlines()
 ## remove whitespace characters like `\n` at the end of each line
   obj_list = [x.strip() for x in obj_list]
-## e.g. first object in the list
-#print(obj_list[0])
-
-# change directory to the one with the files to be changed
-parent_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-parent_path = os.path.abspath(os.path.join(parent_path, os.pardir))
-GT_PATH = os.path.join(parent_path, 'input','ground-truth')
-#print(GT_PATH)
-os.chdir(GT_PATH)
-
-# old files (YOLO format) will be moved to a new folder (backup/)
-## create the backup dir if it doesn't exist already
-if not os.path.exists("backup"):
-  os.makedirs("backup")
 
 # create VOC format files
-txt_list = glob.glob('*.txt')
+txt_list = glob.glob(f'{args.yolo_in}/*.txt')
 if len(txt_list) == 0:
   print("Error: no .txt files found in ground-truth")
-  sys.exit()
+  exit(-1)
+  
 for tmp_file in txt_list:
-  #print(tmp_file)
-  # 1. check that there is an image with that name
-  ## get name before ".txt"
-  image_name = tmp_file.split(".txt",1)[0]
-  #print(image_name)
+  txt_name = str(tmp_file.split("/")[-1])
+  image_name = txt_name.removesuffix(".txt")
+  print(image_name)
   ## check if image exists
-  for fname in os.listdir('../images'):
+  for fname in os.listdir(args.images):
     if fname.startswith(image_name):
       ## image found
-      #print(fname)
-      img = cv2.imread('../images/' + fname)
+      print(fname)
+      img = cv2.imread(args.images + '/' + fname)
       ## get image width and height
       img_height, img_width = img.shape[:2]
       break
@@ -72,10 +62,8 @@ for tmp_file in txt_list:
     content = f.readlines()
   ## remove whitespace characters like `\n` at the end of each line
   content = [x.strip() for x in content]
-  # 3. move old file (YOLO format) to backup
-  os.rename(tmp_file, "backup/" + tmp_file)
-  # 4. create new file (VOC format)
-  with open(tmp_file, "a") as new_f:
+  # 3. create new file (VOC format)
+  with open(args.out + '/' + txt_name, "w") as new_f:
     for line in content:
       ## split a line by spaces.
       ## "c" stands for center and "n" stands for normalized
@@ -85,4 +73,4 @@ for tmp_file in txt_list:
       ## add new line to file
       #print(obj_name + " " + str(left) + " " + str(top) + " " + str(right) + " " + str(bottom))
       new_f.write(obj_name + " " + str(left) + " " + str(top) + " " + str(right) + " " + str(bottom) + '\n')
-print("Conversion completed!")
+print(f"Conversion completed! {len(txt_list)} files")
